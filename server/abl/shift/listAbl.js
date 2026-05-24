532b9bc086fb98b9dab5d75a711a000fc9b14767
LD <damluky@gmail.com>
2026-05-24 23:28:38 +0200

    feat: shift/list vrací workerMap

--- server/abl/shift/listAbl.js
+++ server/abl/shift/listAbl.js
@@ -1,9 +1,17 @@
 const shiftDao = require("../../dao/shift-dao");
+const workerDao = require("../../dao/worker-dao");
 
 async function listAbl(req, res) {
   try {
     const shiftList = await shiftDao.list();
-    res.json(shiftList);
+    const workerList = await workerDao.list();
+
+    const workerMap = {};
+    for (const worker of workerList) {
+      workerMap[worker.id] = worker;
+    }
+
+    res.json({ itemList: shiftList, workerMap });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
